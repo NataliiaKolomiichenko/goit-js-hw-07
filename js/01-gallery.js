2c78f85762b794e75d8b50afc338b3fde1645055
NataliiaKolomiichenko <gella1996nr@gmail.com>
2022-07-17 18:44:07 +0300

    Task 1 - done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const markup = galleryItems.map(({ preview, original, description }) => `<div class="gallery__item"> <a class="gallery__link" href="large-image.jpg"> <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" /> </a> </div>`).join('');
+
+galleryEl.insertAdjacentHTML('afterbegin', markup);
+
+
+galleryEl.addEventListener('click', (event => {
+    const bigImageSrc = event.target.dataset.source;
+
+    const bigImage = basicLightbox.create(`<img src="${bigImageSrc}" width="1280" >`);
+
+    bigImage.show();
+}));
+
+const imageLinks = document.querySelectorAll('.gallery__link');
+
+imageLinks.forEach((link) => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault()
+    });
+});
