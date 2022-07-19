3535c52b9fe8f90a84077a42766fe35233c77525
NataliiaKolomiichenko <gella1996nr@gmail.com>
2022-07-19 14:43:44 +0300

    Task 1 - fixed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -9,17 +9,10 @@ galleryEl.insertAdjacentHTML('afterbegin', markup);
 
 
 galleryEl.addEventListener('click', (event => {
+    event.preventDefault()
     const bigImageSrc = event.target.dataset.source;
 
     const bigImage = basicLightbox.create(`<img src="${bigImageSrc}" width="1280" >`);
 
     bigImage.show();
 }));
-
-const imageLinks = document.querySelectorAll('.gallery__link');
-
-imageLinks.forEach((link) => {
-    link.addEventListener('click', (event) => {
-        event.preventDefault()
-    });
-});
